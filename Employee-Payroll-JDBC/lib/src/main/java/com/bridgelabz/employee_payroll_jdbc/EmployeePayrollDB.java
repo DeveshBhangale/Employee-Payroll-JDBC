b12993b9d3b2f5a5c0129d3890e65dda6436817a
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-19 01:02:34 +0530

    [Devesh]: updated terisa basic pay of  salary

--- Employee-Payroll-JDBC/lib/src/main/java/com/bridgelabz/employee_payroll_jdbc/EmployeePayrollDB.java
+++ Employee-Payroll-JDBC/lib/src/main/java/com/bridgelabz/employee_payroll_jdbc/EmployeePayrollDB.java
@@ -42,7 +42,44 @@ public class EmployeePayrollDB {
 			}
 			Statement stmt = con.createStatement();
 			stmt.executeUpdate(String.format("insert into employee_payroll values (%d,'%s','M',00,'NA','NA',%d,0,0,0,0,0,'%s')",id,name,salary,date));
-			
+		}catch(SQLException e) {e.printStackTrace();}
+	}
+	
+	public void getSalaryByName(Connection con, String name) throws SQLException {
+		try {
+			PreparedStatement stmt=con.prepareStatement(String.format("select salary from employee_payroll where name = '%s'",name)); 
+			ResultSet rs=stmt.executeQuery();
+			while(rs.next())System.out.println(rs.getInt(1));
+		}catch(SQLException e) {e.printStackTrace();}
+	}
+	
+	public void getSalaryByDateRange(Connection con,String fromDate)  throws SQLException {
+		try {
+			PreparedStatement stmt=con.prepareStatement(String.format("SELECT * FROM employee_payroll WHERE start BETWEEN CAST('%s' AS DATE) AND DATE(NOW())",fromDate)); 
+			ResultSet rs=stmt.executeQuery();
+			printData(rs);
+		}catch(SQLException e) {e.printStackTrace();}
+	}
+	
+	public void updateGenderByName(Connection con, String name,String gender) throws SQLException {
+		try {
+			String query ="update employee_payroll set gender=? where name=?";
+			PreparedStatement stmt=con.prepareStatement(query);
+			stmt.setString(1, gender);
+			stmt.setString(2, name);
+			stmt.executeUpdate();
+			System.out.println("Updated !");
+		}catch(SQLException e) {e.printStackTrace();}
+	}
+	
+	public void updateBasePayByName(Connection con, String name,double basePay) throws SQLException {
+		try {
+			String query ="update employee_payroll set basicPay=? where name=?";
+			PreparedStatement stmt=con.prepareStatement(query);
+			stmt.setDouble(1, basePay);
+			stmt.setString(2, name);
+			stmt.executeUpdate();
+			System.out.println("Updated !");
 		}catch(SQLException e) {e.printStackTrace();}
 	}
 	
